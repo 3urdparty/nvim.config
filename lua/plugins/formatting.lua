b7d754a94f005c39f900a34ad4edd44c674dbea7
3urdparty <3urdparty@gmail.com>
2025-01-19 09:28:10 +0800

    Added Astro support

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -3,8 +3,32 @@ return {
     "stevearc/conform.nvim",
     opts = {
       formatters_by_ft = {
+        ["*"] = { "prettier" }, -- Default formatter
+        ["javascript"] = { "prettier" },
+        ["typescript"] = { "prettier" },
+        ["javascriptreact"] = { "prettier" },
+        ["typescriptreact"] = { "prettier" },
+        ["vue"] = { "prettier" },
+        ["css"] = { "prettier" },
+        ["scss"] = { "prettier" },
+        ["less"] = { "prettier" },
         ["html"] = { "prettier" },
+        ["json"] = { "prettier" },
+        ["jsonc"] = { "prettier" },
+        ["astro"] = { "prettier" },
+        ["yaml"] = { "prettier" },
+        ["markdown"] = { "prettier" },
+        ["markdown.mdx"] = { "prettier" },
+        ["graphql"] = { "prettier" },
         ["xml"] = { "prettier" },
+        ["php"] = { "prettier" },
+      },
+      formatters = {
+        prettier = {
+          env = {
+            PRETTIERD_DEFAULT_CONFIG = vim.fn.expand("~/.config/nvim/.prettierrc"),
+          },
+        },
       },
     },
   },
