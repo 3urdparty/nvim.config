2e4f8298f48e8c5918b065a938ae4b0022cbb280
Mustafa Yousif <3urdparty@gmail.com>
2025-05-08 13:44:00 +0800

    Fixed fzf issue which searched files that are in node_modues

--- lua/plugins/base.lua
+++ lua/plugins/base.lua
@@ -1,14 +1,39 @@
+-- ~/.config/nvim/lua/plugins/fzf-lua.lua
+
 return {
+  {
+    "ibhagwan/fzf-lua", -- Ensure this is the plugin specification
+    opts = {
+      files = {
+        command = "rg --files --hidden --ignore-file=.gitignore",
+      },
+    },
+  },
+
+  {
+    "echasnovski/mini.surround",
+    opts = {
+      mappings = {
+        add = "gsa",
+        delete = "gsd",
+        find = "gsf",
+        find_left = "gsF",
+        highlight = "gsh",
+        replace = "gsr",
+        update_n_lines = "gsn",
+      },
+    },
+  },
+
   {
     "stevearc/conform.nvim",
     opts = {
       formatters_by_ft = {
-        ["*"] = { "prettier" }, -- Default formatter
-        ["javascript"] = { "prettier" },
-        ["typescript"] = { "prettier" },
-        ["javascriptreact"] = { "prettier" },
-        ["typescriptreact"] = { "prettier" },
-        ["vue"] = { "prettier" },
+        ["javascript"] = { "eslint-lsp" },
+        ["typescript"] = { "eslint-lsp" },
+        ["javascriptreact"] = { "eslint-lsp" },
+        ["typescriptreact"] = { "eslint-lsp" },
+        ["vue"] = { "eslint-lsp" },
         ["css"] = { "prettier" },
         ["scss"] = { "prettier" },
         ["less"] = { "prettier" },
@@ -22,6 +47,7 @@ return {
         ["graphql"] = { "prettier" },
         ["xml"] = { "prettier" },
         ["php"] = { "prettier" },
+        ["rs"] = { "ast-grep" },
       },
       formatters = {
         prettier = {
